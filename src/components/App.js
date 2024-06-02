9735e6f83db47894ef3cbc8edf984daa5a788351
Kushal Ghosh <kushalghosh075@gmail.com>
2024-06-02 13:40:38 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [sum, setSum] = useState(0);
+  const [number, setNumber] = useState('');
+  const change = (e) => {
+    const val = e.target.value;
+    setNumber(val);
+    setSum(prev => prev + parseInt(val));
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <input type="number" value={number} onChange={change} />
+        <h3>Sum: {sum}</h3>
     </div>
   )
 }
